3fdc578f92d73f3d574dbeacb7a22893d3bd18f4
Gabriel Bezerra <ggbzrr@yahoo.com.br>
2018-05-26 10:44:52 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/gabriel/cursomc/services/ClienteService.java
+++ src/main/java/com/gabriel/cursomc/services/ClienteService.java
@@ -5,24 +5,24 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.gabriel.cursomc.domain.Categoria;
-import com.gabriel.cursomc.repositories.CategoriaRepository;
+import com.gabriel.cursomc.domain.Cliente;
+import com.gabriel.cursomc.repositories.ClienteRepository;
 import com.gabriel.cursomc.services.exceptions.ObjectNotFoundExcepiton;
 
 @Service
-public class CategoriaService {
+public class ClienteService {
 	//responsavel por fazer consult no repository
 	
 	
 	//qndo declara uma dependencia dentro de uma classe	e coloca o autowired, 
 	//essa dependencia sera instanciada automaticamente pelo spring	
 	@Autowired
-	private CategoriaRepository repo;
+	private ClienteRepository repo;
 
-		public Categoria buscar(Integer id) {
-			Optional<Categoria> obj = repo.findById(id);
+		public Cliente buscar(Integer id) {
+			Optional<Cliente> obj = repo.findById(id);
 			
 			return obj.orElseThrow(() -> new ObjectNotFoundExcepiton(
-			"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+			"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
 }
