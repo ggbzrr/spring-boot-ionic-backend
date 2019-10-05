1c38100b83ba5f19b9ace4e12075c5fe410fc337
ggbzrr <ggbzrr@yahoo.com.br>
2019-10-05 10:26:34 -0300

    email HTML

--- src/main/java/com/gabriel/cursomc/services/EmailService.java
+++ src/main/java/com/gabriel/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.gabriel.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.gabriel.cursomc.domain.Pedido;
@@ -8,6 +10,9 @@ import com.gabriel.cursomc.domain.Pedido;
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
-
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
+
 }
